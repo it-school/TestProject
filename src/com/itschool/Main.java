710b68bcf04cebcd18b673dadb0789079a7a4eaf
it-school <it@osvita.od.ua>
2018-09-03 10:47:41 +0300

    Updated cycles

--- src/com/itschool/Main.java
+++ src/com/itschool/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println();
         for (int i = 0; i < 10; i++)
         {
-
+            System.out.println("i = " + i);
         }
 
         int thisIsTheVariable = 0;
