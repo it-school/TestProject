3bba600271dab9f48491f002ea053818179f611a
it-school <it@osvita.od.ua>
2018-09-03 10:40:11 +0300

    Initial commit

---
+++ src/com/itschool/Main.java
@@ -0,0 +1,17 @@
+package com.itschool;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("dghdjG");
+        System.out.println();
+        for (int i = 0; i < 10; i++)
+        {
+
+        }
+
+        int thisIsTheVariable = 0;
+
+
+    }
+}
